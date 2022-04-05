45ee6f2389dd0211e53ec77145e789f01c82648a
avery finchum <finchum.avery@gmail.com>
2022-04-05 09:07:20 -0400

    added comment data

---
+++ 24Hour/24HourComment.Data/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourComment.Data
+{
+    internal class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string CommentText { get; set; }
+        public Guid AuthorId { get; set; }
+
+    }
+}
