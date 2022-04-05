3223f3ddf0435d459dc0b950ec0d77c144e6ebda
Victor Ryan <vryan9615@gmail.com>
2022-04-05 09:49:50 -0400

    Added Post create and Post List Items

---
+++ 24Hour/24HourPost.Model/PostListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourPost.Model
+{
+    public class PostListItem
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+
+        //public string Text { get; set; } Commented out as a precaution
+    }
+}
