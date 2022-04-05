e259bb483e09417d2f6fcd8874aa6fcda6aaa190
Victor Ryan <vryan9615@gmail.com>
2022-04-05 10:51:56 -0400

    Created post controllers

---
+++ 24Hour/RM24HourAPI/Controllers/PostController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using _24HourPost.Services;
+using _24HourPost.Data;
+using _24HourPost.Model;
+
+namespace RM24HourAPI.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new PostService(userId);
+            return noteService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+    }
+    }
+}
