22be27c1c0a3a20681017669de45a2701b0f8bfd
Victor Ryan <vryan9615@gmail.com>
2022-04-05 10:14:23 -0400

    Added certain commands to Post Services, updated IdentityModels

---
+++ 24Hour/24HourPost.Services/PostService.cs
@@ -0,0 +1,59 @@
+﻿using _24HourPost.Data;
+using _24HourPost.Model;
+using RM24HourAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourPost.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    AuthorId = _userId,
+                    Title = model.Title,
+                    Text = model.Text
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Post.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<PostListItem> GetPosts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Posts
+                    .Where(e => e.AuthorId == _userId)
+                    .Select(
+                        e =>
+                        new PostListItem
+                        {
+                            PostId = e.PostId,
+                            Title = e.Title,
+
+                        }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
