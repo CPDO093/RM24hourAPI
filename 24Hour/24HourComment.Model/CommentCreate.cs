0ced3572662aa0b743471484cd1446dc1146cf7a
avery finchum <finchum.avery@gmail.com>
2022-04-05 10:31:12 -0400

    Comment Model creation comment item change

--- 24Hour/24HourComment.Model/CommentCreate.cs
+++ 24Hour/24HourComment.Model/CommentCreate.cs
@@ -7,14 +7,23 @@ using System.Threading.Tasks;
 
 namespace _24HourComment.Model
 {
-    public class CommentItem
+    public class CommentCreate
     {
+        [Required]
         public int CommentId { get; set; }
+
+        [Required]
         public Guid AuthorId { get; set; }
+
+        [Required]
+        [MinLength(2,ErrorMessage ="Please enter at least 2 characters.")]
+        [MaxLength(300,ErrorMessage ="Please limit your comment to 300 characters.")]
         public string CommentText { get; set; }
 
-        [Display(Name="Time Created")]
+        [Required]
         public DateTimeOffset CreatedUtc { get; set; }
+
         public virtual List<Reply> Replies { get; set; }
+
     }
 }
