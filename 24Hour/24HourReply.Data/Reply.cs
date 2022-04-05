224444ac99e7a7ac83d9d280f8ed3c99b8e7cfcf
corey pfleiger <corey.c.pfleiger@gmail.com>
2022-04-05 10:05:27 -0400

    Commit before next step

---
+++ 24Hour/24HourReply.Data/Reply.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourReply.Data
+{
+    internal class Reply
+    {
+        [Key]
+        public int ReplyId { get; set; }
+        [Required]
+        public string RText { get; set; }
+
+    }
+}
---
+++ 24Hour/24HourReply.Model/ReplyCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourReply.Model
+{
+    internal class ReplyCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage="Not enough characters for a reply!")]
+        [MaxLength(250, ErrorMessage ="No one will read this, to many characters for a reply!")]
+        public string RText { get; set; }
+    }
+}
